201a44ceb5c570f8d807abca135bf4e040c9c00f
Rex <proandceo@gmail.com>
2023-03-15 08:22:10 +1000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,19 @@
 from typing import List
 from pathlib import Path
 
+
 class Parser:
-    extensions = []
-    extensions: List[str]
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, 'r') as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext).name
