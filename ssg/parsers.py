291c54f95bd5e25632502ac395794627aa8c8ebc
Rex <proandceo@gmail.com>
2023-03-15 12:15:32 +1000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,3 +38,9 @@ class ResourceParser(Parser):
 
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
